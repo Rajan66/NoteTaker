9b65a06fa0ac4db77e0a2713a3770333e8f24fc4
Rajan66 <rajanmaharjan042@gmail.com>
2023-12-20 13:14:04 +0545

    getNote controller created

--- server/controllers/notes.js
+++ server/controllers/notes.js
@@ -1,1 +1,11 @@
 const Note = require('../models/note')
+
+const getNotes = async (req, res) => {
+    try {
+        const notes = Note.find()
+        res.status(200).json(notes)
+    } catch (error) {
+        res.status(404).json({ message: error.message })
+    }
+}
+
