6b50f69b01675a32821ee2264c4fdb60baa9c5e4
Rajan66 <rajanmaharjan042@gmail.com>
2023-12-20 13:32:53 +0545

    GET for individual note works

--- server/controllers/notes.js
+++ server/controllers/notes.js
@@ -2,7 +2,7 @@ const Note = require('../models/note')
 
 const getNotes = async (req, res) => {
     try {
-        const notes = Note.find()
+        const notes = await Note.find()
         res.status(200).json(notes)
     } catch (error) {
         res.status(404).json({ message: error.message })
@@ -10,7 +10,15 @@ const getNotes = async (req, res) => {
 }
 
 
-const getNote = async (req, res) => { }
+const getNote = async (req, res) => {
+    const { id: _id } = req.params
+    try {
+        const note = await Note.findById(_id)
+        res.status(200).json(note)
+    } catch (error) {
+        res.status(404).json({ message: error.message })
+    }
+}
 
 const createNote = async (req, res) => {
     const note = req.body
