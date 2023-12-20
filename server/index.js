dbee85a4b58a99a75b58333806b5149a893b1fde
Rajan66 <rajanmaharjan042@gmail.com>
2023-12-20 13:05:22 +0545

    database setup

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const dotenv = require('dotenv')
 const cors = require('cors')
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 
 const app = express()
 dotenv.config()
@@ -9,10 +10,13 @@ dotenv.config()
 app.use(cors())
 app.use(bodyParser.json({ extended: false }))
 
+const CONNECTION_URL = process.env.CONNECTION_URL
 const PORT = process.env.PORT || 5000
 
 app.get('/', (req, res) => {
     res.send('Herroo everynyan!🙌🏻')
 })
 
-app.listen(PORT, () => console.log(`Magic is happening on port:${PORT}`))
+mongoose.connect(CONNECTION_URL)
+    .then(() => app.listen(PORT, () => console.log(`Magic is happening on port:${PORT}`)))
+    .catch((error) => console.log(error.message))
