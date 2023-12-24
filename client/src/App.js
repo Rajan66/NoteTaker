4a4a03060ec9c1fc90a4c653f7caada09da1586f
Rajan66 <rajanmaharjan042@gmail.com>
2023-12-24 17:07:20 +0545

    some ui changes

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import {
   Routes
 } from "react-router-dom";
 import './App.css';
+import { Button } from "@mui/material"
 import NotesListPage from './pages/NotesListPage';
 import Header from './components/Header';
 import NotePage from './pages/NotePage';
@@ -15,17 +16,19 @@ import { useEffect, useState } from "react";
 function App() {
   const [auth, setAuth] = useState(false || window.localStorage.getItem('auth') === 'true')
   const [token, setToken] = useState('')
+  const [username, setUsername] = useState('')
 
   useEffect(() => {
     firebase.auth().onAuthStateChanged((userCred) => {
       if (userCred) {
+        setUsername(userCred.displayName)
         setAuth(true)
         window.localStorage.setItem('auth', 'true')
         userCred.getIdToken()
           .then((token) => setToken(token))
       }
     })
-  }, [])
+  }, [username])
 
   const loginWithGoogle = () => {
     firebase.auth()
@@ -33,18 +36,21 @@ function App() {
       .then(
         (userCred) => {
           if (userCred) {
+            setUsername(userCred.displayName)
             setAuth(true)
             window.localStorage.setItem('auth', 'true')
           }
         }
-      )
+      ).catch((error) => {
+        console.log(error.message)
+      })
   }
 
   const handleLogout = () => {
     firebase.auth().signOut()
       .then(() => {
-        console.log('User logged out');
         setAuth(false)
+        setUsername('')
         window.localStorage.setItem('auth', 'false')
       }).catch((error) => {
         console.error('Error during logout:', error);
@@ -55,6 +61,9 @@ function App() {
       {auth ? (
         <Router>
           <div className="container dark">
+            <div className="welcome-user" style={{ marginBottom: "10px" }}>
+              Welcome {username} !📝
+            </div>
             <div className="app">
               <Header />
               <Routes>
@@ -63,13 +72,35 @@ function App() {
               </Routes>
 
             </div>
-            <button style={{ marginTop: "20px" }} >Logout</button>
+            <Button
+              variant="contained"
+              style={{
+                backgroundColor: "var(--color-main)",
+                fontSize: "16px",
+                marginTop: "15px"
+              }}
+              onClick={handleLogout}>
+              Logout
+            </Button>
           </div>
 
         </Router>
       ) : (
-        <div>
-          <button onClick={loginWithGoogle}>Login with Google</button>
+        <div className="container dark" >
+          <div>
+            <h1 style={{ fontSize: "60px" }}>👋🏻 Welcome to NoteTaker 😺 </h1>
+          </div>
+          <Button
+            variant="contained"
+            className="buttons"
+            style={{
+              backgroundColor: "var(--color-main)",
+              fontSize: "16px",
+              marginTop: "55px"
+            }}
+            onClick={loginWithGoogle}>
+            Login with Google
+          </Button>
         </div>
       )}
     </>
