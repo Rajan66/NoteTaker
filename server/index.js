9d45cca12e2e2d2448b6ac59e48909fb72ea995a
Rajan66 <rajanmaharjan042@gmail.com>
2023-12-24 17:11:27 +0545

    removed unused imports

--- server/index.js
+++ server/index.js
@@ -4,7 +4,6 @@ const cors = require('cors')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const noteRoutes = require('./routes/notes')
-const middleware = require('./middleware')
 
 const app = express()
 dotenv.config()
